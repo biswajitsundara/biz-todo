b84d20dc2a93c7e4ee47f208d862015b77dac4b5
Biswajit Sundara <biswajitsundara@gmail.com>
2023-07-23 21:02:34 +0530

    modal context

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,20 @@ import Header from "./components/Header/Header";
 import SideNav from "./components/Sidenav/SideNav";
 import MainContent from "./components/Main/MainContent";
 import Footer from "./components/Footer/Footer";
+import { ModalProvider } from "./Hooks/ModalContext";
+import Modal from "./components/Modal/Modal";
 
 function App() {
   return (
-    <Layout
-      header={<Header />}
-      sideNav={<SideNav />}
-      main={<MainContent />}
-      footer={<Footer />}
-    />
+    <ModalProvider>
+      <Layout
+        header={<Header />}
+        sideNav={<SideNav />}
+        main={<MainContent />}
+        footer={<Footer />}
+      />
+      <Modal />
+    </ModalProvider>
   );
 }
 
